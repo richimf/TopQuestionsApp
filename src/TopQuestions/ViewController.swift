a9a80971c2fa848b7cf09ccdae86066a1729637a
Richie <richimf1@gmail.com>
2022-01-20 15:33:16 -0600

    [FEATURE] Added assets colors and icons

--- src/TopQuestions/ViewController.swift
+++ src/TopQuestions/ViewController.swift
@@ -11,10 +11,8 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .green
+        self.view.backgroundColor = .white
         // Do any additional setup after loading the view.
     }
-
-
 }
 
